80adab9d9eaf8044196c1b3dede268192d2bfb98
Alessandro Frigeri <alessandro.frigeri@gmail.com>
2015-08-19 19:18:06 +0200

    added

--- sillyolimpics/sillyolimpics.py
+++ sillyolimpics/sillyolimpics.py
@@ -3,12 +3,23 @@ import os, ConfigParser
 
 Config = ConfigParser.ConfigParser()
 
-for loc in os.curdir, os.path.expanduser("~/sillyolimpics/"), "/etc/myproject/" :
-    conf = os.path.join(loc,"sillyolimpics.conf")
-    try: 
-	    Config.read(conf)
-    except IOError:
-            pass
+def read_config():
+
+	for loc in os.curdir, os.path.expanduser("~/sillyolimpics/"), "/etc/myproject/" :
+    		conf = os.path.join(loc,"sillyolimpics.conf")
+    	try: 
+	    	Config.read(conf)
+    	except IOError:
+            	pass
+
+class Athlete:
+	def __init__(self):
+		read_config()
+		Config.add_section('athlete')
+		Config.set('athlete', 'an_int', '15')
+		print Config.get('athlete', 'Name')
+		
+	
 
 
 class CTS:
