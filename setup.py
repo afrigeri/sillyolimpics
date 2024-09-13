43480743e363d6d2c6f21066246da31e159cf28a
Alessandro Frigeri <alessandro.frigeri@inaf.it>
2024-09-14 00:21:52 +0200

    prepare for readthedocs

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['tabulate'],
+    install_requires=['tabulate','sphinx-rtd-theme'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
